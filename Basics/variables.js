10ed800736663312ceb0c45d5474b33651583b40
ashutoshpatel01 <160737204+ashutoshpatel01@users.noreply.github.com>
2024-06-29 02:13:19 +0000

    variables

---
+++ Basics/variables.js
@@ -0,0 +1,9 @@
+const myName = "Ashutosh" // can't change
+const rollNo = 23232
+let email = "ashutos@googlr.com"
+var age = 21 // it has scope problem i.e if  another file with same named variable then taht variables get changed
+ idea = "NO" // default take as let
+
+console.log(age)
+
+console.table([myName, rollNo, email, age, idea])
