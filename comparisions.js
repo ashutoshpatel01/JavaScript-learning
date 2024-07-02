428474a618fe3c5b7df3b6dc1c8b4912d8f5b1bd
ashutoshpatel01 <160737204+ashutoshpatel01@users.noreply.github.com>
2024-07-02 01:53:32 +0000

    comaprision

---
+++ comparisions.js
@@ -0,0 +1,16 @@
+console.log(2 > 1) // boolean
+console.log( 3 <= 3) // boolean
+
+
+
+console.log("2" > 1)  // CONERTED TO NUMBER THEN COMPARED
+
+console("02" > 1) // "02" CONERTED TO NUMBER THEN COMPARED
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
