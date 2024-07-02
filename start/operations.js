3f11f38897bf469e55f944452f4ae557edf76e6c
ashutoshpatel01 <160737204+ashutoshpatel01@users.noreply.github.com>
2024-07-02 01:25:21 +0000

    operations

---
+++ start/operations.js
@@ -0,0 +1,25 @@
+// + . / , -, * are some basic oprtn 
+// a**B = a^B
+//a % B modulas oprtn
+
+let str1 = "i'm"
+let str2 = " good"
+let str = str1 + str2
+// console.log(str)
+
+// console.log("1" + 2) //12
+
+// console.log(1 + "2") // 12
+
+// console.log("1" + 2 + 2) //122
+
+// console.log(1 + 2 + "2") // 32
+
+console.log(+true) // for true => true & +true = 1
+
+console.log(+"") // 0
+
+
+
+
+ 
