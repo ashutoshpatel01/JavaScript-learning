7aa6e3621d56cc9cfadf0a431b9b34b32880d881
ashutoshpatel01 <160737204+ashutoshpatel01@users.noreply.github.com>
2024-07-02 01:57:31 +0000

    basics

---
+++ start/datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using nodejs not browser
+
+// maintain code readability 
+
+let name = "ashutosh"
+let nage = 20
+let isLogged = true
+
+
+// number(2^53 range)
+// bigInt
+
+// string   => ' ' or " "
+
+// boolean true/false
+
+// NULL -> it is standalone value
+
+// Undefined => value is not assigned && null is representaion  empty
+
+//symbol => in react (uniqness in react)
+
+// OBJECT 
+
+console.log(typeof "ashutosh")  // tells type 
+
+console.log(typeof NULL)   // NULL is type of OBJECT
+
