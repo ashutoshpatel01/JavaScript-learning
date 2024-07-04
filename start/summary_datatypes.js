98c9cdf08a37331c7fd34e64e4f2d504e8cd847a
ashutoshpatel01 <160737204+ashutoshpatel01@users.noreply.github.com>
2024-07-04 01:58:32 +0000

    Summary datatypes

--- start/summary_datatypes.js
+++ start/summary_datatypes.js
@@ -28,3 +28,41 @@
     console.log("hello World"); // tyoe of data types = "object Function"
   }
 
+
+
+  // ---------------------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------------------------------
+
+
+
+  //STACK AND MEMORY
+
+// Stack(primitive datatype)  . Heap (Non- primitive)
+
+
+let mynmae = "AShutosh patel"
+
+let anotherName = mynmae
+
+anotherName = "patel"
+
+console.log(mynmae)
+console.log(anotherName)
+
+
+// heap (non - primitive)
+let user1=  {
+    email: 'SSKJ932',
+    UPI: "2JKFJ98",
+
+}
+let user2 = user1; // here user2 reference user1
+
+user2.UPI = "hhd23" // here thischange wil incur in user1 also they reference same memeory
+
+
+console.log(user2.UPI) //hhd23
+console.log(user1.UPI)  // hhd23
+
+
+
